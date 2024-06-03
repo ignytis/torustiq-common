1e40332de19636f265736bff5eb6f73cd52e7e66
Ignytis <155588001+ignytis@users.noreply.github.com>
2024-06-04 01:03:25 +0300

    Replace ModuleType with IoKind

--- src/ffi/types/module.rs
+++ src/ffi/types/module.rs
@@ -1,31 +1,58 @@
-use crate::types::module::{Module as RustModule, ModuleType as RustModuleType};
+// use crate::types::module::{Module as RustModule, ModuleType as RustModuleType};
+use crate::types::module::{Module as RustModule, IoKind as RustIoKind};
+
+// #[repr(C)]
+// pub enum ModuleType {
+//     Destination,
+//     Source,
+//     Transofmation,
+// }
+
+
+// impl Into<RustModuleType> for ModuleType {
+//     fn into(self) -> RustModuleType {
+//         match self {
+//             Self::Destination => RustModuleType::Destination,
+//             Self::Source => RustModuleType::Source,
+//             Self::Transofmation => RustModuleType::Transofmation,
+//         }
+//     }
+// }
 
 #[repr(C)]
-pub enum ModuleType {
-    Destination,
-    Source,
-    Transofmation,
+pub enum IoKind {
+    Batch,
+    /// Defines a module as source if set to input.
+    /// Defines a module as destination if set to output.
+    External,
+    Stream,
 }
 
-impl Into<RustModuleType> for ModuleType {
-    fn into(self) -> RustModuleType {
+impl Into<RustIoKind> for IoKind {
+    fn into(self) -> RustIoKind {
         match self {
-            Self::Destination => RustModuleType::Destination,
-            Self::Source => RustModuleType::Source,
-            Self::Transofmation => RustModuleType::Transofmation,
+            Self::Batch => RustIoKind::Batch,
+            Self::External => RustIoKind::External,
+            Self::Stream => RustIoKind::Stream,
         }
     }
 }
 
+/// A module returned by dynamic link library
 #[repr(C)]
 pub struct Module {
-    pub module_type: ModuleType,
+    // /// Is it source, destination, or transformation in between
+    // pub module_type: ModuleType,
+
+    pub input_kind: IoKind,
+    pub output_kind: IoKind,
 }
 
 impl Into<RustModule> for Module {
     fn into(self) -> RustModule {
         RustModule {
-            module_type: self.module_type.into(),
+            input_kind: self.input_kind.into(),
+            output_kind: self.output_kind.into(),
         }
     }
 }
--- src/types/module.rs
+++ src/types/module.rs
@@ -1,9 +1,19 @@
-pub enum ModuleType {
-    Source,
-    Transofmation,
-    Destination,
+// pub enum ModuleType {
+//     Source,
+//     Transofmation,
+//     Destination,
+// }
+
+pub enum IoKind {
+    Batch,
+    /// Defines a module as source if set to input.
+    /// Defines a module as destination if set to output.
+    External,
+    Stream,
 }
 
 pub struct Module {
-    pub module_type: ModuleType,
+    // pub module_type: ModuleType,
+    pub input_kind: IoKind,
+    pub output_kind: IoKind,
 }
