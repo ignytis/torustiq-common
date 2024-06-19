50d4bf6cc865c2a8871822e1c73da8a16b7c8e1e
Ignytis <155588001+ignytis@users.noreply.github.com>
2024-06-19 22:13:26 +0300

    Style fixes

--- src/ffi/types/functions.rs
+++ src/ffi/types/functions.rs
@@ -2,11 +2,10 @@ use crate::ffi::types::module::ModuleInfo;
 
 use super::{module::{ModuleInitStepArgs, Record}, std_types};
 
-// TODO: add the payload size and use it in string initialization. 
-// Null-terminated strings are most likely not safe, as we can receive the binary data
-// Perhaps should use arrays of numbers instead?
-pub type ModuleOnDataReceivedFn = extern fn(Record);
-
+// The following functions are expected to be exported by libraries
 pub type ModuleGetInfoFn = extern fn() -> ModuleInfo;
 pub type ModuleInitStepFn = extern fn(ModuleInitStepArgs);
+
+// These are callback functions
+pub type ModuleOnDataReceivedFn = extern fn(Record);
 pub type ModuleTerminationHandlerFn = extern fn(std_types::Uint);
