e383fe7f1446cfdace2b01fc3644fa6b36b7ae4c
Ignytis <155588001+ignytis@users.noreply.github.com>
2024-06-23 16:37:33 +0300

    Pathed the FROM method for byte buffers

--- src/ffi/types/buffer.rs
+++ src/ffi/types/buffer.rs
@@ -8,8 +8,10 @@ impl ByteBuffer {
     pub fn get_bytes_as_const_ptr<T>(&self) -> *const T {
         unsafe { std::mem::transmute(self.bytes) }
     }
+}
 
-    pub fn from_string(input: String) -> Self {
+impl From<String> for ByteBuffer {
+    fn from(input: String) -> Self {
         let mut dst: Vec<u8> = Vec::with_capacity(input.len());
         unsafe { std::ptr::copy(input.as_ptr(), dst.as_mut_ptr(), input.len()) };
         let bytes = dst.as_mut_ptr();
@@ -22,6 +24,7 @@ impl ByteBuffer {
     }
 }
 
+// TODO: check for memory leaks and start using this function
 pub extern "C" fn free_buf(buf: ByteBuffer) {
     let s = unsafe { std::slice::from_raw_parts_mut(buf.bytes, buf.len) };
     let s = s.as_mut_ptr();
