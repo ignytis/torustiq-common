23786b175bf84d518090791be65901c33caee073
Ignytis <155588001+ignytis@users.noreply.github.com>
2024-06-26 23:18:08 +0300

    Defined a const pointer to C string

--- src/ffi/types/std_types.rs
+++ src/ffi/types/std_types.rs
@@ -2,5 +2,6 @@ use core::ffi::{c_char, c_uint};
 
 pub type ConstBytePtr = *const u8;
 pub type ConstCharPtr = *const c_char;
+pub type ConstCStrPtr = *const i8;
 pub type Char = c_char;
 pub type Uint = c_uint;
