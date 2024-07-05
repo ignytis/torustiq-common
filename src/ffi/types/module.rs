8e348aaba82e9915608b0097716683e164b133b7
Ignytis <155588001+ignytis@users.noreply.github.com>
2024-07-06 01:52:49 +0300

    Pipeline step kind and init result

--- src/ffi/types/module.rs
+++ src/ffi/types/module.rs
@@ -4,29 +4,33 @@ use super::{buffer::ByteBuffer, collections::Array, functions::{
     ModuleOnDataReceivedFn, ModuleTerminationHandlerFn
 }};
 
-#[derive(Clone, Debug, PartialEq)]
-#[repr(C)]
-pub enum IoKind {
-    Batch,
-    /// Defines a module as source if set to input
-    /// Defines a module as destination if set to output.
-    External,
-    Stream,
-}
-
 /// Module information
 #[derive(Clone)]
 #[repr(C)]
 pub struct ModuleInfo {
     pub id: std_types::ConstCharPtr,
     pub name: std_types::ConstCharPtr,
-    pub input_kind: IoKind,
-    pub output_kind: IoKind,
 }
+
+/// Specifies the position of step in pipeline
+#[derive(Clone, PartialEq)]
+#[repr(C)]
+pub enum PipelineStepKind {
+    /// Source: produces the data itself, no input from other steps is expected.
+    Source,
+    /// Transformation: gets the data from the previous step (source or transformation)
+    /// and sends the processed result to the next step
+    Transformation,
+    /// Destination: a final point in the pipeline. Receives the data, but doesn't send it
+    /// to any further step
+    Destination,
+}
+
 
 /// Arguments passed to init function
 #[repr(C)]
 pub struct ModuleStepInitArgs {
+    pub kind: PipelineStepKind,
     pub step_handle: ModuleStepHandle,
     pub termination_handler: ModuleTerminationHandlerFn,
 
@@ -60,7 +64,20 @@ unsafe impl Send for Record {}
 
 pub type ModuleStepHandle = std_types::Uint;
 
+/// Returns the status of module initialization
+#[repr(C)]
+pub enum ModuleStepInitFnResult {
+    /// Initialization succeeded
+    Ok,
+    /// The provided kind (source, transformation, destination) is not supported by module.
+    /// Modules don't necessarily can handle all kinds of steps
+    ErrorKindNotSupported,
+    /// Other kind of error occurred. More details in text message
+    ErrorMisc(std_types::ConstCharPtr),
+}
+
 // TODO: review if any result is needed to return at all
+// TODO2: probably just need to report if data transfer is succeeded
 #[repr(C)]
 pub enum ModuleProcessRecordFnResult {
     /// No output. Typical for destination module
