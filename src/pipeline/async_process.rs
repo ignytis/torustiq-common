e53b82b0ce2e62eb884b3e6fb334936666bd1264
Ignytis <155588001+ignytis@users.noreply.github.com>
2024-11-05 00:17:02 +0200

    A function which creates an instance of sender and receiver

--- src/pipeline/async_process.rs
+++ src/pipeline/async_process.rs
@@ -1,9 +1,6 @@
-use std::{collections::HashMap, sync::{mpsc::{Receiver, Sender}, Mutex}};
-
-use crate::ffi::types::module::{ModuleStepHandle, ModuleProcessRecordFnResult, Record};
-
+use std::{collections::HashMap, sync::{mpsc::{Receiver, Sender, channel}, Mutex}};
 use once_cell::sync::Lazy;
-
+use crate::ffi::types::module::{ModuleStepHandle, ModuleProcessRecordFnResult, Record};
 
 pub static RECORD_SENDERS: Lazy<Mutex<HashMap<ModuleStepHandle, Sender<Record>>>> = Lazy::new(|| {
     Mutex::new(HashMap::new())
@@ -26,7 +23,7 @@ extern "C" fn torustiq_module_process_record(in_record: Record, step_handle: Mod
     ModuleProcessRecordFnResult::Ok
 }
 
-
+/// Extracts a receiver object from the map and returns it
 pub fn get_receiver_owned(handle: ModuleStepHandle) -> Option<Receiver<Record>> {
     match RECORD_RECEIVERS.lock().unwrap().remove(&handle) {
         Some(r) => Some(r),
@@ -34,7 +31,9 @@ pub fn get_receiver_owned(handle: ModuleStepHandle) -> Option<Receiver<Record>> 
     }
 }
 
-
-pub fn add_receiver(handle: ModuleStepHandle, receiver: Receiver<Record>) {
-    RECORD_RECEIVERS.lock().unwrap().insert(handle, receiver);
+/// Creates a sender and a receiver; stores them inside module maps
+pub fn create_sender_and_receiver(step_handle: ModuleStepHandle) {
+    let (sender, receiver) = channel::<Record>();
+    RECORD_RECEIVERS.lock().unwrap().insert(step_handle, receiver);
+    RECORD_SENDERS.lock().unwrap().insert(step_handle, sender);
 }
